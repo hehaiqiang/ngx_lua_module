ffd01b33e5cec0f4aea10926e692de4657c43ecf
hqhe <hqhe1982@126.com>
2011-08-04 01:05:52 +0800

    *) compiling some http modules.

--- makefile-unix.mk
+++ makefile-unix.mk
@@ -14,28 +14,13 @@ CONF_ARGS= \
 	$(PCRE_CONF_ARGS) \
 	$(HTTP_CONF_ARGS) \
 	--without-http-cache \
-	--without-http_charset_module \
 	--without-http_gzip_module \
-	--without-http_ssi_module \
-	--without-http_userid_module \
-	--without-http_access_module \
 	--without-http_auth_basic_module \
-	--without-http_autoindex_module \
-	--without-http_status_module \
-	--without-http_geo_module \
-	--without-http_map_module \
-	--without-http_split_clients_module \
-	--without-http_referer_module \
 	--without-http_proxy_module \
 	--without-http_fastcgi_module \
 	--without-http_uwsgi_module \
 	--without-http_scgi_module \
 	--without-http_memcached_module \
-	--without-http_limit_zone_module \
-	--without-http_limit_req_module \
-	--without-http_empty_gif_module \
-	--without-http_browser_module \
-	--without-http_upstream_ip_hash_module \
 	--add-module=$(ADDON_DIR)/../ngx_dbd_module \
 	--add-module=$(ADDON_DIR)
 
