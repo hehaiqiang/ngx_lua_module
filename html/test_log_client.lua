4a314effaa0f38f00ebc912336447eaac5fa1cad
hqhe <hqhe1982@126.com>
2011-12-26 13:27:47 +0800

    Fix empty request and user agent.

--- html/test_log_client.lua
+++ html/test_log_client.lua
@@ -8,17 +8,19 @@ local s, errstr = socket.open('127.0.0.1:8080', socket.UDP)
 if not s then log.error(log.ALERT, errstr) return end
 
 local remote_user = var.remote_user or ''
+local request = var.request or ''
 local http_referer = var.http_referer or ''
+local http_user_agent = var.http_user_agent or ''
 
 local str = '{'
   .. 'remote_addr="' .. var.remote_addr .. '",'
   .. 'remote_user="' .. remote_user .. '",'
   .. 'time_local="' .. var.time_local .. '",'
-  .. 'request="' .. var.request .. '",'
+  .. 'request="' .. request .. '",'
   .. 'status="' .. var.status .. '",'
   .. 'body_bytes_sent="' .. var.body_bytes_sent .. '",'
   .. 'http_referer="' .. http_referer .. '",'
-  .. 'http_user_agent="' .. var.http_user_agent .. '",'
+  .. 'http_user_agent="' .. http_user_agent .. '",'
   .. '}'
 
 s:send(str)
--- html/test_log_server.lua
+++ html/test_log_server.lua
@@ -11,11 +11,11 @@ local t = func()
 if t.remote_user == '' then t.remote_user = '-' end
 if t.http_referer == '' then t.http_referer = '-' end
 
-----[[
+--[[
 local rc, errstr = db.create_db_pool()
 if not rc then log.error(log.ALERT, errstr) return end
 
 local res, errstr = db.insert_logs(t.remote_addr, t.remote_user, t.time_local,
   t.request, t.status, t.body_bytes_sent, t.http_referer, t.http_user_agent)
 if not res then log.error(log.ALERT, errstr) return end
-----]]
+--]]
